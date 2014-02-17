5f8f7204817738dcd3af40efb4b953bfac6d235e
Jimmy Hillis <jimmy@hillis.me>
2014-02-17 21:19:22 +0800

    Map IRC usernames to slack users using API support for link_names

--- bot.js
+++ bot.js
@@ -9,11 +9,30 @@ var slacker = new slack.Slacker({
     token: 'xoxp-2175856885-2175856887-2176637470-9337ab'
 });
 
+var USERMAP = {
+    'jimmyhillis': 'jimmyhillis',
+    'niaal': 'niaal',
+    'simon___': 'simon',
+    'san_dy': 'sandy'
+};
+
+
 client.addListener('message', function (from, to, message) {
-    console.log(from + ' => ' + to + ': ' + message);
     slacker.send('chat.postMessage', {
         channel: '#irc',
-        text: mapUsers(message),
-        username: from
+        text: mapUsers(message, USERMAP),
+        username: USERMAP[from] || from,
+        parse: 'full',
+        link_names: 1,
+        unfurl_links: 1
     });
 });
+
+var mapUsers = function (message, users) {
+    Object.keys(users).forEach(function (name) {
+        if (message.indexOf(name) >= 0) {
+            message = message.replace(new RegExp(name, 'g'), '@' + users[name]);
+        }
+    });
+    return message;
+};
