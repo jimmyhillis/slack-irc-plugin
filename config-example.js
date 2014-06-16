4cdd69b7b16cc2d2d10ecedd6b526558cd346c3e
Jimmy Hillis <jimmy@hillis.me>
2014-06-16 20:00:17 +0800

    Fix typo in configuration

--- config-example.js
+++ config-example.js
@@ -3,7 +3,7 @@ var slackbot = require('./lib/bot');
 var config = {
     server: 'irc.freenode.com',
     nick: 'slackbot',
-    username: 'slackbot-username'
+    username: 'slackbot-username',
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
     channels: {
         '#irc-channel password(optional)': '#slack-channel'
